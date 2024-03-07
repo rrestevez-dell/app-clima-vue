9cac0741adcda85217465823b981f8e63046f37d
RAY <delirestevez@gmail.com>
2024-03-07 22:50:39 +0100

    fix

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -14,7 +14,7 @@ export default function useClima() {
         try {
             //obtener latitud y longitud
 
-            const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
+            const url = `https://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
             const { data } = await axios(url)
             const { lat, lon } = data[0]
               
